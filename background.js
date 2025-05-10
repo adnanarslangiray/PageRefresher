2f3419b2d23594dcfa0ac3858b59aa4d26ef0fe1
adnan <adnan@ADNAN-PC>
2025-05-10 16:52:00 +0300

    added icons folder

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'notify') {
     chrome.notifications.create({
       type: 'basic',
-      iconUrl: 'icon48.png',
+      iconUrl: 'icons/icon48.png',
       title: 'Text Watcher',
       message: message.text,
       priority: 2
